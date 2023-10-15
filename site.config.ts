563c5962692f2f2ae07c91a91800c903c72ad207
Ramiro <ramiro@kryha.io>
2023-10-15 18:11:48 +0200

    Update site config

--- site.config.ts
+++ site.config.ts
@@ -2,24 +2,23 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
-
+  rootNotionPageId: '9e4fd07b82b94a2b8103587b1c8a30a4',
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Ramiro writes Tech - Blog',
+  domain: 'ramirotech.blog',
+  author: 'Ramiro Ramirez',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Exploring technology',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'eco_virtual',
+  github: 'ecovirtual',
+  linkedin: 'ramirezramiro/?locale=en_US',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
